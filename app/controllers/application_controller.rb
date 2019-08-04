1f41fe72d6b88020d701b6ae16dbc5e6bf06b8d7
Ryan Gilfillan <regilfillan@gmail.com>
2019-08-04 20:31:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
   get "/" do
-    erb :
+    erb :super_hero
+  end
+  
+  post "/teams" do 
+    erb :team
   end
 
 end
