ea9aea7275230df24aa5d5bcdefd85dc8a1959b6
Ryan Gilfillan <regilfillan@gmail.com>
2019-08-04 20:58:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,5 +23,14 @@ class App < Sinatra::Base
     
   #   erb :team
   # end
+  
+  post "/teams" do
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+      members = params[:team][:members]
+      @super_heroes = members.collect do |member_params|
+        SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+      end
+      erb :team
+    end
 
 end
